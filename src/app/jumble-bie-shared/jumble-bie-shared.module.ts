34b5578fa258b8bd640eab3c7d7c3cd2db38e7be
Neeraj Khandelwal <neerajk251991@gmail.com>
2018-10-06 17:48:42 +0530

    Resolved

--- src/app/jumble-bie-shared/jumble-bie-shared.module.ts
+++ src/app/jumble-bie-shared/jumble-bie-shared.module.ts
@@ -5,12 +5,43 @@ import { SlickCarouselForHomeComponent } from './slick-carousel-for-home/slick-c
 import { SlickCarouselForDetailComponent } from './slick-carousel-for-detail/slick-carousel-for-detail.component';
 import { JbModalComponent, JbFooter } from './jb-modal/jb-modal.component';
 import { RatingComponent } from './rating/rating.component';
-
+import { NgBusyModule, BusyConfig, BUSY_CONFIG_DEFAULTS } from 'ng-busy';
+import { CustomBusyComponent } from './custom-busy/custom-busy.component';
+const busyConfig: BusyConfig = {
+    message: BUSY_CONFIG_DEFAULTS.message,
+    backdrop: true,
+    template: CustomBusyComponent,
+    delay: 200,
+    minDuration: 600,
+    wrapperClass: BUSY_CONFIG_DEFAULTS.wrapperClass,
+    disableAnimation: false
+};
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        NgBusyModule.forRoot(busyConfig)
     ],
-    declarations: [SlickCarouselForHomeComponent, PriceSliderComponent, SlickCarouselForDetailComponent, JbFooter, JbModalComponent, RatingComponent],
-    exports: [SlickCarouselForHomeComponent, PriceSliderComponent, SlickCarouselForDetailComponent, JbFooter, JbModalComponent, RatingComponent]
+    declarations: [
+        SlickCarouselForHomeComponent,
+        PriceSliderComponent,
+        SlickCarouselForDetailComponent,
+        JbFooter,
+        JbModalComponent,
+        CustomBusyComponent,
+        RatingComponent
+    ],
+    entryComponents: [
+        CustomBusyComponent
+    ],
+    exports: [
+        SlickCarouselForHomeComponent,
+        PriceSliderComponent,
+        SlickCarouselForDetailComponent,
+        JbFooter,
+        JbModalComponent,
+        NgBusyModule,
+        CustomBusyComponent,
+        RatingComponent
+    ]
 })
 export class JumbleBieSharedModule { }
