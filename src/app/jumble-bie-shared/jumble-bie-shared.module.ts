b981f30e8082a2d904b942a04dc2397af1c16c7a
Neeraj Khandelwal <neerajk251991@gmail.com>
2018-10-06 17:26:00 +0530

    Added rating component

--- src/app/jumble-bie-shared/jumble-bie-shared.module.ts
+++ src/app/jumble-bie-shared/jumble-bie-shared.module.ts
@@ -4,12 +4,13 @@ import { PriceSliderComponent } from './price-slider/price-slider.component';
 import { SlickCarouselForHomeComponent } from './slick-carousel-for-home/slick-carousel-for-home.component';
 import { SlickCarouselForDetailComponent } from './slick-carousel-for-detail/slick-carousel-for-detail.component';
 import { JbModalComponent, JbFooter } from './jb-modal/jb-modal.component';
+import { RatingComponent } from './rating/rating.component';
 
 @NgModule({
     imports: [
         CommonModule
     ],
-    declarations: [SlickCarouselForHomeComponent, PriceSliderComponent, SlickCarouselForDetailComponent, JbFooter, JbModalComponent],
-    exports: [SlickCarouselForHomeComponent, PriceSliderComponent, SlickCarouselForDetailComponent, JbFooter, JbModalComponent]
+    declarations: [SlickCarouselForHomeComponent, PriceSliderComponent, SlickCarouselForDetailComponent, JbFooter, JbModalComponent, RatingComponent],
+    exports: [SlickCarouselForHomeComponent, PriceSliderComponent, SlickCarouselForDetailComponent, JbFooter, JbModalComponent, RatingComponent]
 })
 export class JumbleBieSharedModule { }
